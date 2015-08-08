0eaab8365111dba6e68bb02049ea0c7682d7c37e
Asbjørn Apeland <aude@users.noreply.github.com>
2015-08-08 12:28:28 +0200

    space to tab in meta blocks

--- dist/tek.no-freedom.meta.js
+++ dist/tek.no-freedom.meta.js
@@ -1,22 +1,22 @@
 // ==UserScript==
-// @name        Tek.no: freedom
-// @namespace   https://github.com/aude
-// @author      aude
+// @name	Tek.no: freedom
+// @namespace	https://github.com/aude
+// @author	aude
 // @description Tek.no can be a little invasive to our private space. Good thing it's our private space.
-// @include     /^https?://(www\.)?(tek|gamer|teknofil|insidetelecom)\.no(/.*)?$/
-// @domain      tek.no
-// @domain      gamer.no
-// @domain      teknofil.no
-// @domain      insidetelecom.no
-// @match       http://www.tek.no/*
-// @match       https://www.tek.no/*
-// @match       http://www.gamer.no/*
-// @match       https://www.gamer.no/*
-// @match       http://www.teknofil.no/*
-// @match       https://www.teknofil.no/*
-// @match       http://www.insidetelecom.no/*
-// @match       https://www.insidetelecom.no/*
-// @version     0.5
-// @grant       none
-// @run-at      document-start
+// @include	/^https?://(www\.)?(tek|gamer|teknofil|insidetelecom)\.no(/.*)?$/
+// @domain	tek.no
+// @domain	gamer.no
+// @domain	teknofil.no
+// @domain	insidetelecom.no
+// @match	http://www.tek.no/*
+// @match	https://www.tek.no/*
+// @match	http://www.gamer.no/*
+// @match	https://www.gamer.no/*
+// @match	http://www.teknofil.no/*
+// @match	https://www.teknofil.no/*
+// @match	http://www.insidetelecom.no/*
+// @match	https://www.insidetelecom.no/*
+// @version	0.5
+// @grant	none
+// @run-at	document-start
 // ==/UserScript==
--- dist/tek.no-freedom.user.js
+++ dist/tek.no-freedom.user.js
@@ -1,30 +1,30 @@
 // ==UserScript==
-// @name        Tek.no: freedom
-// @namespace   https://github.com/aude
-// @author      aude
+// @name	Tek.no: freedom
+// @namespace	https://github.com/aude
+// @author	aude
 // @description Tek.no can be a little invasive to our private space. Good thing it's our private space.
-// @include     /^https?://(www\.)?(tek|gamer|teknofil|insidetelecom)\.no(/.*)?$/
-// @domain      tek.no
-// @domain      gamer.no
-// @domain      teknofil.no
-// @domain      insidetelecom.no
-// @match       http://www.tek.no/*
-// @match       https://www.tek.no/*
-// @match       http://www.gamer.no/*
-// @match       https://www.gamer.no/*
-// @match       http://www.teknofil.no/*
-// @match       https://www.teknofil.no/*
-// @match       http://www.insidetelecom.no/*
-// @match       https://www.insidetelecom.no/*
-// @version     0.5
-// @grant       none
-// @run-at      document-start
+// @include	/^https?://(www\.)?(tek|gamer|teknofil|insidetelecom)\.no(/.*)?$/
+// @domain	tek.no
+// @domain	gamer.no
+// @domain	teknofil.no
+// @domain	insidetelecom.no
+// @match	http://www.tek.no/*
+// @match	https://www.tek.no/*
+// @match	http://www.gamer.no/*
+// @match	https://www.gamer.no/*
+// @match	http://www.teknofil.no/*
+// @match	https://www.teknofil.no/*
+// @match	http://www.insidetelecom.no/*
+// @match	https://www.insidetelecom.no/*
+// @version	0.5
+// @grant	none
+// @run-at	document-start
 // ==/UserScript==
 
 // ==ChangeLog==
-// @history     0.5 GitHub migration
-// @history     0.4 initial release
-// @history     0.1 dark ages
+// @history	0.5 GitHub migration
+// @history	0.4 initial release
+// @history	0.1 dark ages
 // ==/ChangeLog==
 
 // ==License==
